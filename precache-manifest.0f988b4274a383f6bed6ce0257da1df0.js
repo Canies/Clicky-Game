cf289589955275a5ba8da72bc3e0f2dd9f54e55c
Bianca <canisenelson@gmail.com>
2020-01-14 21:17:57 -0500

    Updates

---
+++ precache-manifest.0f988b4274a383f6bed6ce0257da1df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342b2e19ebdfd5f0c4e5f27e14d6c820",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4470c129cd9c6c965c00",
+    "url": "/Clicky-Game/static/css/main.42298a23.chunk.css"
+  },
+  {
+    "revision": "d9d2f085dc1282096077",
+    "url": "/Clicky-Game/static/js/2.a318ea46.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.a318ea46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4470c129cd9c6c965c00",
+    "url": "/Clicky-Game/static/js/main.bff8afdd.chunk.js"
+  },
+  {
+    "revision": "dc0770571834f315f918",
+    "url": "/Clicky-Game/static/js/runtime-main.559ed188.js"
+  }
+]);
